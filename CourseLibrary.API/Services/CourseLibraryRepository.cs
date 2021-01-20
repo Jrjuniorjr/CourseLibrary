bb2bae5b2bab4a355ca5fe53eca2438e593935da
Jrjuniorjr <mjuniorjrjr@outlook.com>
2021-01-19 22:11:56 -0300

    Pagination.

--- CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -125,12 +125,6 @@ namespace CourseLibrary.API.Services
                 throw new ArgumentNullException(nameof(authorsResourceParameters));
             }
 
-            if (string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory)
-                && string.IsNullOrWhiteSpace(authorsResourceParameters.SearchQuery))
-            {
-                return GetAuthors();
-            }
-
             var collection = _context.Authors as IQueryable<Author>;
 
             if (!string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory))
@@ -146,7 +140,10 @@ namespace CourseLibrary.API.Services
                 || a.FirstName.Contains(searchQuery) || a.LastName.Contains(searchQuery));
             }
 
-            return collection.ToList();
+            return collection
+                .Skip(authorsResourceParameters.PageSize * (authorsResourceParameters.PageNumber - 1))
+                .Take(authorsResourceParameters.PageSize)
+                .ToList();
         }
 
         public IEnumerable<Author> GetAuthors()
