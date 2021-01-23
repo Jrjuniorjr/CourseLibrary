5f2cd8e1ebba7b2c79ba6bb557f92a38b449a636
Jrjuniorjr <mjuniorjrjr@outlook.com>
2021-01-22 21:31:21 -0300

    Correção PagedList

--- CourseLibrary.API/Helpers/PagedList.cs
+++ CourseLibrary.API/Helpers/PagedList.cs
@@ -12,7 +12,7 @@ namespace CourseLibrary.API.Helpers
         public int PageSize { get; private set; }
         public int TotalCount { get; private set; }
         public bool HasPrevious => (CurrentPage > 1);
-        public bool HasNext => (CurrentPage < 1);
+        public bool HasNext => (CurrentPage < TotalPages);
 
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
